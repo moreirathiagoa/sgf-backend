fb4f51046400e08640c945ca1dd3a17837b27cc5
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2020-04-21 13:23:11 -0300

    Testes de deploy (1)

--- src/properties/index.js
+++ src/properties/index.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
-dotenv.config();
+//const dotenv = require('dotenv');
+//dotenv.config();
 
 const URI_DATABASE = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@sgfcluster-nrl0f.gcp.mongodb.net/test?retryWrites=true&w=majority`
 
