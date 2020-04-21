ad621e69e0a5c9a470c91b85e15c092d4fa0cd09
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2020-04-21 13:43:35 -0300

    Testes de deploy (3)

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const port = process.env.PORT || 4000
 const app = require('./src/app')
