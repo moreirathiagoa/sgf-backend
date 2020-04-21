2f54a3edd7ca9684c2ca65b3f1f87d2f2f944eb3
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2020-04-21 14:03:56 -0300

    Configuração do ambiente ininicial

--- src/properties/index.js
+++ src/properties/index.js
@@ -1,4 +1,4 @@
-const URI_DATABASE = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@sgfcluster-nrl0f.gcp.mongodb.net/test?retryWrites=true&w=majority`
+const URI_DATABASE = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@sgfcluster-nrl0f.gcp.mongodb.net/sgf?retryWrites=true&w=majority`
 
 module.exports = {
     URI_DATABASE
