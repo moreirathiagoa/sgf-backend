26e008d9f1239f412547430c1c039eebb0e0e3f2
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2020-04-21 22:07:57 -0300

    ajuse no DataBase

--- src/properties/index.js
+++ src/properties/index.js
@@ -2,7 +2,7 @@ const DB_USERNAME = process.env.DB_USERNAME
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DATA_BASE = process.env.DATA_BASE || 'test'
 
-const URI_DATABASE = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@sgfcluster-nrl0f.gcp.mongodb.net/${DATA_BASE}?retryWrites=true&w=majority`
+const URI_DATABASE = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@sgfcluster-nrl0f.mongodb.net/${DATA_BASE}?retryWrites=true&w=majority`
 
 module.exports = {
     URI_DATABASE
