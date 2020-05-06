1c2a0fce40c7fb5d32722fa9e37de9b992f539b8
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2020-05-05 21:55:45 -0300

    Melhorias no sistema de login

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,21 +5,28 @@ const properties = require('../properties')
 const auth = (req, res, next) => {
 
     const tokenHeader = req.headers.auth
-    
+
     if (!tokenHeader) {
         console.log('token não informado');
         res.status(401).send(utils.makeResponse(401, 'token não informado'))
     }
-    
-    const keyToken= properties.keyToken    
 
-    jwt.verify(tokenHeader, keyToken, (err, decoded)=>{
+    const keyToken = properties.keyToken
+
+    jwt.verify(tokenHeader, keyToken, { ignoreExpiration: true }, (err, decoded) => {
         if (err) {
             res.status(401).send(utils.makeResponse(401, 'token inválido'))
         } else {
-            res.locals.authData = decoded
+            var current_time = Date.now() / 1000;
+            if (decoded.exp < current_time && !decoded.trustComputer) {
+                res.status(401).send(utils.makeResponse(401, 'token inválido'))
+            } else {
+                //TODO: Verificar se Token está autorizado login permanente no mongo
+
+                res.locals.authData = decoded
+                return next()
+            }
         }
-        return next()
     })
 }
 
