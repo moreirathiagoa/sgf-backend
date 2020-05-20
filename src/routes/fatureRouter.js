a8eb79d78e8c4196b99e918316008bb269ee7446
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2020-05-19 21:38:04 -0300

    HOTFIX: list de transaçao sem filtro

--- src/routes/fatureRouter.js
+++ src/routes/fatureRouter.js
@@ -6,9 +6,10 @@ const auth = require('../middlewares/auth')
 
 router.get('/list/:bank_id', auth, async (req, res, next) => {
     global.userId = res.locals.authData.userId
-
+    const { bank_id } = req.params
     try {
-        const response = await controller.fatureController.getListFatures()
+        const response = await controller.fatureController.getListFatures(bank_id)
+        console.log(response)
         res.status(response.code).send(response)
     } catch (error) {
         res.status(500).send(error)
