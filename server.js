067c711ac3f9261527d9e5f4b3fc1eb91fae1643
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2020-06-04 20:35:51 -0300

    teste porta

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 40000
 const app = require('./src/app')
 const dbConnection = require('./src/database')
 
