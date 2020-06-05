d526ed97862948931920dd0f057301e1b8c6351a
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2020-06-05 14:20:46 -0300

    Ajustes da porta

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-const port = process.env.PORT || 47807
+const port = process.env.PORT || 4000
 const app = require('./src/app')
 const dbConnection = require('./src/database')
 
