04068d3ed609d77102910243a09fa406ba82da8f
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2021-09-17 18:26:22 -0300

    Configurado conexão com o banco de dados

--- server.js
+++ server.js
@@ -6,16 +6,13 @@ const port = process.env.PORT || 4000
 const app = require('./src/app')
 const dbConnection = require('./src/database')
 
-dbConnection
-	.start()
-	.then(() => {
-		app.listen(port, () => {
-			console.log(
-				`Servido iniciado na porta ${port} e ambiente ${properties.env.toUpperCase()}`
-			)
-		})
-	})
-	.catch((err) => {
-		console.log(`Erro no início da aplicação: ${err}`)
-		process.exit()
-	})
+dbConnection.start().catch((err) => {
+	console.log(`Erro no início da aplicação: ${err}`)
+	process.exit()
+})
+
+app.listen(port, () => {
+	console.log(
+		`Servido iniciado na porta ${port} e ambiente ${properties.env.toUpperCase()}`
+	)
+})
