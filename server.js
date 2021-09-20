d37ebacdeab860c869f41dcb41308572dea43893
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2021-09-20 08:42:29 -0300

    removendo importação database n

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const port = process.env.PORT || 4000
 const app = require('./src/app')
-const dbConnection = require('./src/database')
 
 app.listen(port, () => {
 	console.log(`Server is running at port: ${port}`)
