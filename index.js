c27f9da5b48884ba8d4a51009299f8a315ca02de
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2022-05-14 16:55:52 -0300

    rollback

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const serverlessExpress = require('@vendia/serverless-express')
 const app = require('./src/app')
-const db = require('./src/database')
 
-exports.handler = serverlessExpress({ app }).then(() => {
-	db.mongoose.connection.close()
-})
+exports.handler = serverlessExpress({ app })
