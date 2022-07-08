9d866683ffddd5cdd873e9145240027ddd7df42c
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2022-07-08 09:10:29 -0300

    removida politica para testes

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,22 +2,7 @@ resource "aws_s3_bucket" "s3Bucket" {
      bucket = "sgf-test"
      acl       = "public-read"
 
-     policy  = <<EOF
-{
-     "id" : "MakePublic",
-   "version" : "2012-10-17",
-   "statement" : [
-      {
-         "action" : [
-             "s3:GetObject"
-          ],
-         "effect" : "Allow",
-         "resource" : "arn:aws:s3:::sgf-test/*",
-         "principal" : "*"
-      }
-    ]
-  }
-EOF
+
 
    website {
        index_document = "index.html"
