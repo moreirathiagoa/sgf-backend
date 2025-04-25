7fd3d9282b02a9855e5ad644292777ebd6232904
Thiago Moreira <thiago.arrudas@olxbr.com>
2025-04-25 19:10:26 -0300

    Implementing amount history feature

--- src/routes/balancesDashboardRouter.js
+++ src/routes/balancesDashboardRouter.js
@@ -1,40 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../middlewares/auth')
-const controllerBank = require('../controllers/bankController')
-const transactionController = require('../controllers/transactionController')
+const balancesDashboardController = require('../controllers/balancesDashboardController')
 
 router.get('/get-balances', auth, async (req, res, next) => {
 	try {
 		const userId = res.locals.authData.userId
-
-		const dashboardDataPromise = [
-			controllerBank.getListBanksDashboard(userId),
-			transactionController.transactionNotCompensatedCredit(userId),
-			transactionController.transactionNotCompensatedDebit(userId),
-		]
-
-		const dashboardData = await Promise.all(dashboardDataPromise)
-		validateResponses(dashboardData)
-
-		const response = {
-			banksList: dashboardData[0].data,
-			balanceNotCompensatedCredit: dashboardData[1].data,
-			balanceNotCompensatedDebit: dashboardData[2].data,
-		}
-
+		const response = await balancesDashboardController.getDetalhesSaldos(userId)
 		res.status(200).json(response)
 	} catch (error) {
 		res.status(500).json(error.message)
 	}
 })
-
-function validateResponses(dashboardData) {
-	const responseCodes = dashboardData.map((el) => el.code)
-	const hasResponseWithError = responseCodes.find((el) => el != 200)
-	if (hasResponseWithError) {
-		throw new Error('Não foi possível obter os dados do dashboard')
-	}
-}
 
 module.exports = router
